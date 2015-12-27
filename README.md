56d7895b7dd711fb0d712a7b7136f5cbc45da9f6
Brian Wilson <wilson@bubba.org>
2015-12-26 21:46:47 -0500

    Took out dependencies on middle-man php script since you can post auth data on GET URL for ST.  Thanks @davglass (https://github.com/davglass/st-beacon-bacon/blob/master/beacon.bacon.groovy)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ GeoHopper Virtual Presence Updater for SmartThings
 GeoHopper is an app for iOS and Android that uses either device GPS or
 Bluetooth LE to GeoHopper
 [iBeacons](http://store.twocanoes.com/collections/ibeacons). This device
-driver, php script and SmartApp will allow you to use Bluetooth on your mobile
+driver and SmartApp will allow you to use Bluetooth on your mobile
 device to set presence in SmartThings. This means you can now get accurate
 presence without using GPS and without using a presence sensor (which also
 requires a SmartThings hub nearby). 
@@ -15,9 +15,6 @@ To get started you'll need:
 - [GeoHopper](https://itunes.apple.com/us/app/geohopper/id605160102?mt=8).
 - An iBeacon (or you can use GPS in the GeoHopper; however why wouldn't you just use SmartThings
   app for GPS presence?). 
-- A webserver. Required to run a .php script that interfaces between GeoHopper and
-  SmartThings (because GeoHopper web scripts cannot send header auth data to
-SmartThings).
 - SmartThings Hub
 	- [A Virtual Presence Device](https://github.com/ajpri/STApps/blob/master/devicetypes/ajpri/virtual-mobile-presence.src/virtual-mobile-presence.groovy)
 	- A SmartApp assigned to your Virtual Presence Device
@@ -37,21 +34,26 @@ back to App Settings and enable OAuth. Update your app.
 4. Launch the Editor for your new SmartApp, Save, Publish, Set the location,
 Select your new device created in step 2 and click install.
 5. Grab the API Token and Endpoint URL from the bottom of the simulator window. 
-6. Find a webserver where you can run PHP code from.  Edit geohopper.php.  Set
-'''authkey''' to be something you can type into your GeoHopper app.  Set
-'''access_key''' to be your API Token from step 4 and set '''endpoint_id'''
-to be the token at the end of your Endpoint URL from step 5 (not the whole URL,
-just the long token at the end). 
-7. Install GeoHopper app and your iBeacon.  In the app, go to Settings->Web
-Service and configure a new web service:
-http://your_web_server/geohopper.php?auth=your_auth_key. Select "Post". You can
+6. Copy the Endpoint URL and add '''/location/?access_token=[API Token From
+Above]'''.  This will be your URL to configure in GeoHopper.  It should look
+something like this:
+https://graph.api.smartthings.com/api/smartapps/installations/2654dfb4-0c85-4025-xxxxx-xxxxx/location/?access_token=9c7bc742-19d6-4cc9-80a6-xxxxxxxxxxx
+7. Now paste this URL into your browser and make sure you get the following
+response:
+<code>
+{"error":true,"type":"SmartAppException","message":"Yep, this is the right URL."}
+</code>
+8. Install GeoHopper app and your iBeacon.  In the app, go to Settings->Web
+Service and configure a new web service using the URL above and 
+select "Post". You can
 select Execute Locally if the server will be on the same network your mobile
 device is on when the triggers need to run. 
-8. Configure a GeoHopper location or rename your iBeacon to be whatever the
+9. Configure a GeoHopper location or rename your iBeacon to be whatever the
 name of your device was in step 2.  For this location, enable your web service
 and perform a test.  It should perform the "On" or "Present" action (check your
 SmartThings IDE to make sure you see the logs rolling in.  If not, make sure
-you have your authcode set on both the geohopper.php and in the GeoHopper app.  
+you have your URL correct in the GeoHopper app. Make sure step 7 completed
+correctly.
 9. You should now have a virtual presence sensor that you can tie to
 SmartThings actions. 
 
--- geohopper_presence-SmartApp.groovy
+++ geohopper_presence-SmartApp.groovy
@@ -3,14 +3,18 @@
  *
  *  Copyright 2015 Brian Wilson
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License
+ *  for the specific language governing permissions and limitations under the
+License.
  *
  */
 definition(
@@ -19,15 +23,19 @@ definition(
     author: "Brian Wilson",
     description: "Geohopper API Presence App",
     category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconUrl:
+"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url:
+"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url:
+"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     oauth: true)
 
 
 preferences {
   section ("Allow external service to control these things...") {
-    input "presence", "capability.presenceSensor", multiple: true, required: true
+    input "presence", "capability.presenceSensor", multiple: true, required:
+true
   }
 }
 
@@ -49,7 +57,8 @@ def initialize() {
 def listLocations() {
     def resp = []
     presence.each {
-      log.debug "RECEIVED: ${it.displayName}, attribute ${it.name}, ID: ${it.id}"
+      log.debug "RECEIVED: ${it.displayName}, attribute ${it.name}, ID:
+${it.id}"
       resp << [Name: it.displayName, ID: it.id]
     }
     return resp
@@ -57,21 +66,29 @@ def listLocations() {
 
 def deviceHandler(evt) {}
 
+void correctURL () {
+	 httpError(200, "Yep, this is the right URL.")
+}
+
 void updateLocation() {
     update(presence)
 }
-private void update(devices) {
-    log.debug "update, request: params: ${params}, devices: $devices.id"
-    def location = params.location
-    def action = params.action
-    def device = devices.find { it.displayName == params.location }
-    log.debug "update, request: params: ${params}, devices: $devices.id, $devices.displayName"
 
-    if (location){
+private void update (devices) {
+   def data = request.JSON
+   def location = data.location
+   def event = data.event
+   def device = devices.find { it.displayName == location }
+   log.debug "update, request: params: ${params}, data, ${data}, devices:
+$devices.id, $devices.displayName"
+   log.debug "event: ${event} device: ${device} location: ${location}"
+   
+   if (location){
         if (!device) {
+        	log.debug "Error: device (${device}) not found"
             httpError(404, "Device not found")
-        } else {
-            if(action == "LocationExit"){
+     	} else {
+            if(event == "LocationExit"){
                   device.off();
             } else { 
                   device.on();
@@ -86,10 +103,10 @@ mappings {
             GET: "listLocations"
         ]
     }
-    path("/location/:location/:action") {
-        action: [
-            GET: "updateLocation"
+    path("/location") {
+    	action: [
+        	POST: "updateLocation",
+            GET: "correctURL"
         ]
     }
 }
-// TODO: implement event handlers
