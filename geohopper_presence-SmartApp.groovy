6929d974fe766eae19c9024a1ac63561035b85e7
Brian Wilson <wilson@bubba.org>
2017-12-17 09:45:08 -0500

    fixed wrapped lines

--- geohopper_presence-SmartApp.groovy
+++ geohopper_presence-SmartApp.groovy
@@ -3,18 +3,14 @@
  *
  *  Copyright 2015 Brian Wilson
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-use this file except
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-express or implied. See the License
- *  for the specific language governing permissions and limitations under the
-License.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License 
+ *  for the specific language governing permissions and limitations under the License.
  *
  */
 definition(
@@ -23,19 +19,15 @@ definition(
     author: "Brian Wilson",
     description: "Geohopper API Presence App",
     category: "My Apps",
-    iconUrl:
-"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url:
-"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url:
-"https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     oauth: true)
 
 
 preferences {
   section ("Allow external service to control these things...") {
-    input "presence", "capability.presenceSensor", multiple: true, required:
-true
+    input "presence", "capability.presenceSensor", multiple: true, required: true
   }
 }
 
@@ -57,8 +49,7 @@ def initialize() {
 def listLocations() {
     def resp = []
     presence.each {
-      log.debug "RECEIVED: ${it.displayName}, attribute ${it.name}, ID:
-${it.id}"
+      log.debug "RECEIVED: ${it.displayName}, attribute ${it.name}, ID: ${it.id}"
       resp << [Name: it.displayName, ID: it.id]
     }
     return resp
@@ -79,8 +70,7 @@ private void update (devices) {
    def location = data.location
    def event = data.event
    def device = devices.find { it.displayName == location }
-   log.debug "update, request: params: ${params}, data, ${data}, devices:
-$devices.id, $devices.displayName"
+   log.debug "update, request: params: ${params}, data, ${data}, devices: $devices.id, $devices.displayName"
    log.debug "event: ${event} device: ${device} location: ${location}"
    
    if (location){
